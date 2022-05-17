372d1ceb1cea5b29d4413dae1034b0eb0b323e73
wh28533 <wh28533@gmail.com>
2022-05-17 18:08:51 +0900

    modified the file : client.cc

---
+++ client.cc
@@ -0,0 +1,28 @@
+#include "ns3/log.h"
+#include "ns3/ipv4-address.h"
+#include "ns3/address-utils.h"
+#include "ns3/nstime.h"
+#include "ns3/inet-socket-address.h"
+#include "ns3/socket.h"
+#include "ns3/udp-socket.h"
+#include "ns3/simulator.h"
+#include "ns3/socket-factory.h"
+#include "ns3/packet.h"
+#include "ns3/uinteger.h"
+#include "ns3/seq-ts-header.h"
+#include "ns3/double.h"
+#include "ns3/boolean.h"
+
+#include "streaming-client.h"
+#include <iostream>
+
+namespace ns3 {
+
+NS_LOG_COMPONENT_DEFINE ("StreamingClientApplication");
+
+NS_OBJECT_ENSURE_REGISTERED (Client);
+
+
+
+
+}
