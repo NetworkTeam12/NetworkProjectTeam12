10a7a11a2758085ef1616ae0433eb99a757f7fec
wh28533 <wh28533@gmail.com>
2022-05-17 17:58:12 +0900

    modified the file : streamer.h

--- streamer.h
+++ streamer.h
@@ -25,7 +25,21 @@ namespace ns3 {
 			void ReSendPacket (uint32_t seqN);
 			void HandleRead (Ptr<Socket> socket);
 			
-			
+			// Attribute 
+			Address m_peerAddress;	// Destination address
+			uint16_t m_peerPort;	// Destination port
+			uint32_t m_packetSize 	// Packet size
+			uint32_t m_fps;			// Streaming FPS
+			bool m_lossEnable;		// Loss Enable 
+			double m_lossRate;		// Loss Rate
+			uint32_t m_packetNIP; 	// Number of packets in Frame
+
+			// Additional member variables
+			Ptr<Socket> m_socket;	// My socket
+			EventId m_sendEvent;	// Send event
+			uint32_t m_frameN; 	// Frame number attached to the packet header
+			uint32_t m_seqN;	// Sequence number attached to the packet header
+
 
 	};
 
